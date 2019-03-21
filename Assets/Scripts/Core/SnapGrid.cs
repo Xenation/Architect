a6a05c6aba3a30620ad7fd72d90d2f1157f0af80
Xenation <alfred.brisac@gmail.com>
2019-03-21 12:46:25 +0100

    Basic SnapGrid bounds

--- Assets/Scripts/Core/SnapGrid.cs
+++ Assets/Scripts/Core/SnapGrid.cs
@@ -1,17 +1,30 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Architect {
 	public class SnapGrid : MonoBehaviour {
 
 		public float snap = 0.1f;
+		public List<Recti> boundsTest = new List<Recti>();
 
 		private Matrix4x4 wtl;
 		private Matrix4x4 ltw;
 
+		private List<Recti> boundingRects = new List<Recti>();
+
+		private MeshFilter filter;
+		private Mesh mesh;
+
 		private float gridHeight { get { return transform.position.y; } }
 
 		private void Awake() {
+			filter = GetComponent<MeshFilter>();
+			mesh = filter.mesh = new Mesh();
 			RecalculateMatrices();
+			foreach (Recti rect in boundsTest) {
+				boundingRects.Add(rect);
+			}
+			RecreateMesh();
 		}
 
 		private void Update() {
@@ -23,11 +36,48 @@ namespace Architect {
 			wtl = ltw.inverse;
 		}
 
+		private void RecreateMesh() {
+			mesh.Clear();
+
+			Vector3[] verts = new Vector3[boundingRects.Count * 4];
+			int[] indices = new int[boundingRects.Count * 6];
+			for (int i = 0; i < boundingRects.Count; i++) {
+				Debug.Log("recti min=" + boundingRects[i].min + "    max=" + boundingRects[i].max);
+				Vector3 min = boundingRects[i].min.Unflat(0).Float() * snap;
+				Vector3 max = boundingRects[i].max.Unflat(0).Float() * snap;
+				Debug.Log("rect min=" + min + "    max=" + max);
+				verts[i * 4] = new Vector3(min.x, min.y, min.z);
+				verts[i * 4 + 1] = new Vector3(min.x, min.y, max.z);
+				verts[i * 4 + 2] = new Vector3(max.x, min.y, max.z);
+				verts[i * 4 + 3] = new Vector3(max.x, min.y, min.z);
+				indices[i * 6] = i * 4;
+				indices[i * 6 + 1] = i * 4 + 1;
+				indices[i * 6 + 2] = i * 4 + 2;
+				indices[i * 6 + 3] = i * 4;
+				indices[i * 6 + 4] = i * 4 + 2;
+				indices[i * 6 + 5] = i * 4 + 3;
+			}
+
+			mesh.vertices = verts;
+			mesh.SetIndices(indices, MeshTopology.Triangles, 0);
+		}
+
+		public void AddBoundingRect(Recti rect) {
+			boundingRects.Add(rect);
+			RecreateMesh();
+		}
+
 		public Vector3 SnapPosition(Vector3 pos) {
-			pos = wtl.MultiplyPoint3x4(pos);
-			pos.y = 0;
-			pos = ((pos / snap).Flat().Round() * snap).Unflat(0);
-			pos = ltw.MultiplyPoint3x4(pos);
+			//pos = wtl.MultiplyPoint3x4(pos);
+			//pos.y = 0;
+			//Vector2Int gridPos = (pos / snap).Flat().RoundToInt();
+			Vector2Int gridPos = WorldToGrid(pos);
+			if (!IsInGrid(gridPos)) {
+				gridPos = ProjectInGrid(gridPos);
+			}
+			pos = GridToWorld(gridPos);
+			//pos = (gridPos.Float() * snap).Unflat(0);
+			//pos = ltw.MultiplyPoint3x4(pos);
 			return pos;
 		}
 
@@ -37,6 +87,37 @@ namespace Architect {
 			euler.y = Mathf.Round(euler.y / 90f) * 90f;
 			euler.y += transform.rotation.eulerAngles.y;
 			return Quaternion.Euler(euler);
+		}
+
+		public bool IsInGrid(Vector2Int gridPos) {
+			foreach (Recti rect in boundingRects) {
+				if (rect.Contains(gridPos)) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public Vector2Int ProjectInGrid(Vector2Int gridPos) {
+			if (boundingRects.Count == 0) return gridPos;
+			Vector2Int closest = gridPos.Clamp(boundingRects[0]);
+
+			for (int i = 1; i < boundingRects.Count; i++) {
+				Vector2Int currentClosest = gridPos.Clamp(boundingRects[i]);
+				if ((gridPos - closest).sqrMagnitude > (gridPos - currentClosest).sqrMagnitude) {
+					closest = currentClosest;
+				}
+			}
+
+			return closest;
+		}
+
+		public Vector2Int WorldToGrid(Vector3 worldPos) {
+			return (wtl.MultiplyPoint3x4(worldPos) / snap).Flat().RoundToInt();
+		}
+
+		public Vector3 GridToWorld(Vector2Int gridPos) {
+			return ltw.MultiplyPoint3x4((gridPos.Float() * snap).Unflat(0));
 		}
 
 	}
--- Assets/Scripts/Utils/MathUtils.cs
+++ Assets/Scripts/Utils/MathUtils.cs
@@ -7,13 +7,22 @@ namespace Architect {
 		public static Vector2 Ceil(this Vector2 v) {
 			return new Vector2(Mathf.Ceil(v.x), Mathf.Ceil(v.y));
 		}
+		public static Vector2Int CeilToInt(this Vector2 v) {
+			return new Vector2Int(Mathf.CeilToInt(v.x), Mathf.CeilToInt(v.y));
+		}
 
 		public static Vector2 Floor(this Vector2 v) {
 			return new Vector2(Mathf.Floor(v.x), Mathf.Floor(v.y));
 		}
+		public static Vector2Int FloorToInt(this Vector2 v) {
+			return new Vector2Int(Mathf.FloorToInt(v.x), Mathf.FloorToInt(v.y));
+		}
 
 		public static Vector2 Round(this Vector2 v) {
 			return new Vector2(Mathf.Round(v.x), Mathf.Round(v.y));
+		}
+		public static Vector2Int RoundToInt(this Vector2 v) {
+			return new Vector2Int(Mathf.RoundToInt(v.x), Mathf.RoundToInt(v.y));
 		}
 
 		public static Vector3 Unflat(this Vector2 v, float y) {
@@ -37,5 +46,64 @@ namespace Architect {
 			return new Vector2(v.x, v.z);
 		}
 
+		// VECTOR2INT
+		public static Vector2Int Clamp(this Vector2Int v, int min, int max) {
+			return new Vector2Int((v.x < min) ? min : ((v.x > max) ? max : v.x), (v.y < min) ? min : ((v.y > max) ? max : v.y));
+		}
+		public static Vector2Int Clamp(this Vector2Int v, Recti rect) {
+			return new Vector2Int((v.x < rect.min.x) ? rect.min.x : ((v.x > rect.max.x) ? rect.max.x : v.x), (v.y < rect.min.y) ? rect.min.y : ((v.y > rect.max.y) ? rect.max.y : v.y));
+		}
+		
+		public static Vector3Int Unflat(this Vector2Int v, int y) {
+			return new Vector3Int(v.x, y, v.y);
+		}
+		public static Vector2 Float(this Vector2Int v) {
+			return new Vector2(v.x, v.y);
+		}
+
+		// VECTOR3INT
+		public static Vector3Int Clamp(this Vector3Int v, int min, int max) {
+			return new Vector3Int((v.x < min) ? min : ((v.x > max) ? max : v.x), (v.y < min) ? min : ((v.y > max) ? max : v.y), (v.z < min) ? min : ((v.z > max) ? max : v.z));
+		}
+		public static Vector3Int Clamp(this Vector3Int v, Boxi box) {
+			return new Vector3Int((v.x < box.min.x) ? box.min.x : ((v.x > box.max.x) ? box.max.x : v.x), (v.y < box.min.y) ? box.min.y : ((v.y > box.max.y) ? box.max.y : v.y), (v.z < box.min.z) ? box.min.z : ((v.z > box.max.z) ? box.max.z : v.z));
+		}
+
+		public static Vector2Int Flat(this Vector3Int v) {
+			return new Vector2Int(v.x, v.z);
+		}
+		public static Vector3 Float(this Vector3Int v) {
+			return new Vector3(v.x, v.y, v.z);
+		}
+
+	}
+
+	[System.Serializable]
+	public struct Recti {
+		public Vector2Int min;
+		public Vector2Int max;
+
+		public Recti(Vector2Int min, Vector2Int max) {
+			this.min = min;
+			this.max = max;
+		}
+
+		public bool Contains(Vector2Int pos) {
+			return !(pos.x < min.x || pos.y < min.y || pos.x > max.x || pos.y > max.y);
+		}
+	}
+
+	public struct Boxi {
+		public Vector3Int min;
+		public Vector3Int max;
+
+		public Boxi(Vector3Int min, Vector3Int max) {
+			this.min = min;
+			this.max = max;
+		}
+
+		public bool Contains(Vector3Int pos) {
+			return !(pos.x < min.x || pos.y < min.y || pos.z < min.z || pos.x > max.x || pos.y > max.y || pos.z > max.z);
+		}
 	}
 }
