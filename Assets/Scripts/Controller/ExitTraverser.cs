47f4469ac2d54c22cfb7a3bb75b1ec3096a1f78d
Xenation <alfred.brisac.auditeur@lecnam.net>
2019-04-29 19:30:00 +0200

    Basic Level End

---
+++ Assets/Scripts/Controller/ExitTraverser.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Valve.VR;
+
+namespace Architect {
+	public class ExitTraverser : LinkTraverser {
+
+		private SteamVR_LoadLevel levelLoader;
+
+		public ExitTraverser(RoomLink link, RoomNetwork roomnet, SteamVR_LoadLevel levelLoader) {
+			this.link = link;
+			this.roomnet = roomnet;
+			this.levelLoader = levelLoader;
+		}
+
+		public override bool Traverse(SilhouetteController controller) {
+			Vector3 toTarget = target - controller.transform.localPosition;
+			Vector3 direction = toTarget.normalized;
+			controller.transform.localPosition += direction * controller.speed * Time.deltaTime;
+			controller.transform.localRotation = Quaternion.Euler(0f, Vector3.SignedAngle(Vector3.forward, direction, Vector3.up), 0f);
+			if (toTarget.magnitude < 0.01f) {
+				levelLoader.Trigger();
+				return true;
+			}
+			return false;
+		}
+
+	}
+}
